4a5b2a9b5aacf06b93da5de1add33b0e856c99f1
Crowdbotics <engineering@crowdbotics.com>
2021-08-17 14:21:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dan3142_2_2Identifier",
+  url: "https://dan3142_2_2Identifier.botics.co",
+  api: "https://dan3142_2_2Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
